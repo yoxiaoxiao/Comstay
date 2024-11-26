fc6e3ad20f68ff221f518022ef0b07b7bc113846
yoxiaoxiao <xiaoxiaofu2866@gmail.com>
2024-11-26 11:37:30 -0800

    Redux store setuo tested

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,10 @@ import configureStore from './store';
 
 const store = configureStore();
 
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
