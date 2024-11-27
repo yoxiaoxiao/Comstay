b58a833b612a1e306434e074afb57fc382c8eaa1
yoxiaoxiao <xiaoxiaofu2866@gmail.com>
2024-11-27 00:53:00 -0800

    Navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import { Outlet, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
 import SignupFormPage from './components/SignupFormPage';
+import Navigation from './components/Navigation';
 import * as sessionActions from './store/session';
 
 function Layout() {
@@ -17,6 +18,7 @@ function Layout() {
 
   return (
     <>
+      <Navigation isLoaded={isLoaded} />
       {isLoaded && <Outlet />}
     </>
   );
@@ -31,11 +33,11 @@ const router = createBrowserRouter([
         element: <h1>Welcome!</h1>
       },
       {
-        path: '/login',
+        path: "login",
         element: <LoginFormPage />
       },
       {
-        path: "/signup",
+        path: "signup",
         element: <SignupFormPage />
       }
     ]
