c929da7a78d23f090aab598597999a1b6dc73f7a
yoxiaoxiao <xiaoxiaofu2866@gmail.com>
2024-11-30 13:07:08 -0800

    Application Header

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,24 +1,25 @@
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+import logo from '../../../public/logo.png';
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
 
   return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-      {isLoaded && (
-        <li>
-          <ProfileButton user={sessionUser} />
-        </li>
-      )}
-    </ul>
+    <nav className="nav-bar">
+      <div className="logo-container">
+        <Link to='/'>
+          <img src={logo} alt="Comstay Logo" />
+        </Link>
+      </div>
+
+      <div className="profile-button-container">
+        {isLoaded && <ProfileButton user={sessionUser} />}
+      </div>
+    </nav>
   );
 }
 
 export default Navigation;
-  
--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -42,7 +42,7 @@ function ProfileButton({ user }) {
 
   return (
     <>
-      <button onClick={toggleMenu}>
+      <button className="profile-button" onClick={toggleMenu}>
         <FaUserCircle />
       </button>
       <ul className={ulClassName} ref={ulRef}>
